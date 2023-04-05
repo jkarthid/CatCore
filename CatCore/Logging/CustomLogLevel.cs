3618e887a30d044dc2b084f28dd6506687e1ab21
Administrator <WIN-N322HOSBH2K\Administrator>
2023-04-05 06:44:45 +0000

    Reworked Credentials handling
    git-tfs-id: [http://192.168.3.197:8080/tfs/DefaultCollection]$/CatCore/cleanup;C554

--- CatCore/Logging/CustomLogLevel.cs
+++ CatCore/Logging/CustomLogLevel.cs
@@ -2,11 +2,11 @@
 {
     public enum CustomLogLevel
     {
-	    Trace = 0,
-        Debug = 1,
+        Debug = 10,
         Information = 2,
         Warning = 3,
         Error = 4,
         Critical = 5
+		EqualityComparer=6
     }
 }
